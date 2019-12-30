b4326b824bc40c278154bb9857b04cb8dd852bfc
semihayucekaya <semihaakca61@yahoo.com>
2019-12-29 23:25:38 -0500

    notes created page

--- src/test/java/com/Odoo/Pages/NotesPage.java
+++ src/test/java/com/Odoo/Pages/NotesPage.java
@@ -1,12 +1,21 @@
 package com.Odoo.Pages;
 
+import com.Odoo.Utitlities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NotesPage {
-    @FindBy(css = "[title='Notes']")
+
+    @FindBy(linkText = "Notes")
     public WebElement Notes;
 
+    @FindBy(xpath = "//ol[@class='breadcrumb']")
+    public WebElement subTitle;
+    
     @FindBy(css = "[class='btn btn-primary btn-sm o-kanban-button-new']")
     public WebElement createButton;
 
@@ -25,9 +34,33 @@ public class NotesPage {
     @FindBy(css = "[class='o_thread_message_content']")
     public WebElement alertMessage;
 
+public void clickNotes(){
+    Notes.click();
 
+}
+    public static String getTitle(String titleText) /*throws InterruptedException*/ {
+        WebDriverWait wait =  new WebDriverWait(Driver.get(), 15);
+        wait.until(ExpectedConditions.textToBe(By.xpath("//ol[@class='breadcrumb']/li"),titleText));
+        //Thread.sleep(5000);
+        WebElement title = Driver.get().findElement(By.xpath("//ol[@class='breadcrumb']/li"));
+        return title.getText();
+    }
 
+    public void clickCreateButton(){
+        createButton.click();
+    }
 
+    public void chooseTags(){
+      TagsDropdown.sendKeys("Tester", Keys.ENTER);
+    }
+
+     public  void writeText(){
+        WritingPanel.sendKeys("This meeting very important",Keys.ENTER);
+     }
+
+     public void clickSaveButton(){
+       saveButton.click();
+     }
 
 
 
--- target/default-cucumber-reports/report.js
+++ target/default-cucumber-reports/report.js
@@ -20,7 +20,7 @@ formatter.scenario({
   ]
 });
 formatter.before({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "user is on the login page",
@@ -30,41 +30,49 @@ formatter.match({
   "location": "loginStepDefinitions.user_is_on_the_login_page()"
 });
 formatter.result({
-  "status": "skipped"
+  "status": "passed"
 });
 formatter.step({
   "name": "user logs in as username",
   "keyword": "And "
 });
-formatter.match({});
+formatter.match({
+  "location": "NotesStepDefinition.user_logs_in_as_username()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.step({
   "name": "user click on \"Notes\" title",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "NotesStepDefinition.user_click_on_title(String)"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.step({
   "name": "user click on \"Create\" button",
   "keyword": "And "
 });
-formatter.match({});
+formatter.match({
+  "location": "NotesStepDefinition.user_click_on_button(String)"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.step({
   "name": "user create new note",
   "keyword": "Then "
 });
-formatter.match({});
+formatter.match({
+  "location": "NotesStepDefinition.user_create_new_note()"
+});
 formatter.result({
-  "status": "undefined"
+  "status": "passed"
 });
 formatter.after({
-  "status": "skipped"
+  "status": "passed"
 });
 });
